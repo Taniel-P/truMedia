7027ff274deeb0f3098465039e0494636c8560cd
Taniel Pogharian <tanielpogharian@Taniels-MacBook-Pro.local>
2022-02-22 13:37:53 -0700

    env

--- server/serverRoutes/player.js
+++ server/serverRoutes/player.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 const key = require('../../config.js').USER_ADDRESS;
+require('dotenv').config();
 const { getToken } = require('../helpers/getToken');
 
 
 module.exports = {
   player: (req, res) => {
-    getToken(key)
+    getToken(process.env.USER_ADDRESS)
     .then((token) => {
       axios.get(`https://project.trumedianetworks.com/api/mlb/player/${req.headers.params}`, {
         headers: {
--- server/serverRoutes/players.js
+++ server/serverRoutes/players.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 const key = require('../../config.js').USER_ADDRESS;
+require('dotenv').config();
 const { getToken } = require('../helpers/getToken');
 
 
 module.exports = {
   players: (req, res) => {
-    getToken(key)
+    getToken(process.env.USER_ADDRESS)
     .then((token) => {
       axios.get('https://project.trumedianetworks.com/api/mlb/players', {
         headers: {
